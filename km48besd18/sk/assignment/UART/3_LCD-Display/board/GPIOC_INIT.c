c76f7949ba163d3c71f87260c9a3d992b7c2a916
sreekanth <s2sreekanth233@gmail.com>
2023-04-26 15:37:37 +0530

    keil-cube

--- km48besd18/sk/assignment/UART/3_LCD-Display/board/GPIOC_INIT.c
+++
@@ -1,9 +0,0 @@
-
-
-#include "myheader.h"
-
-void GPIOC_INIT(void)
-{
-	RCC_AHB1ENR|=(0x1<<2);
-	GPIOC_PUPDR|=0x00050000;
-}
--- km48besd18/sk/assignment/UART/3_LCD-Display/board/NVIC_INIT.c
+++
@@ -1,9 +0,0 @@
-
-
-#include "myheader.h"
-
-void NVIC_INIT(void)
-{
-	NVIC_ISER0|=(0x1<<23);//ENABLING EXTI INTERRUPT HANDLER
-	NVIC_ISER1|=(0x1<<5);//ENABLING USART INTERRUPT HANDLER
-}
